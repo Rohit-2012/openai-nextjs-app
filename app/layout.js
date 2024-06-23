e83071bad9b33055631e84244a336301be0811d7
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-06-23 20:57:38 +0530

    added user profile page & toast component

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,22 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Providers from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'TourGPT',
+  title: "TourGPT",
   description:
-    'TourGPT: Your AI language companion. Powered by OpenAI, it enhances your conversations, content creation, and more!',
+    "TourGPT: Your AI language companion. Powered by OpenAI, it enhances your conversations, content creation, and more!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Providers>{children}</Providers>
+        </body>
       </html>
     </ClerkProvider>
   );
