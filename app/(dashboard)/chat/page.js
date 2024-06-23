0c6fc91fd29a63c8e7079640cf8fd8c0dadf1ba0
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-06-23 22:46:35 +0530

    chat structure and react query setup

--- app/(dashboard)/chat/page.js
+++ app/(dashboard)/chat/page.js
@@ -1,8 +1,14 @@
+import Chat from "@/components/Chat";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+
+
+
 const ChatPage = () => {
+    const queryClient = new QueryClient()
     return (
-        <div>
-            ChatPage
-        </div>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Chat />
+        </HydrationBoundary>
     );
 }
 
