bfa981e16cf9ba0b15d2a832841c142884f19d15
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-06-24 15:10:01 +0530

    created tour, new tour and openAI API

---
+++ components/NewTour.jsx
@@ -0,0 +1,67 @@
+"use client";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  getExistingTour,
+  generateTourResponse,
+  createNewTour,
+} from "@/utils/action";
+import TourInfo from "./TourInfo";
+import toast from "react-hot-toast";
+
+const NewTour = () => {
+  const {
+    mutate,
+    isPending,
+    data: tour,
+  } = useMutation({
+    mutationFn: async (destination) => {
+      const newTour = await generateTourResponse(destination);
+      if (newTour) {
+        return newTour;
+      }
+      toast.error("No matching destination found...");
+      return null;
+    },
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const destination = Object.fromEntries(formData.entries());
+    mutate(destination);
+  };
+
+  if (isPending) {
+    return <span className="loading loading-lg"></span>;
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="max-w-2xl">
+        <h2 className="mb-4">Select your destination</h2>
+        <div className="join w-full">
+          <input
+            type="text"
+            className="input input-bordered join-item w-full"
+            placeholder="city"
+            name="city"
+            required
+          />
+          <input
+            type="text"
+            className="input input-bordered join-item w-full"
+            placeholder="country"
+            name="country"
+            required
+          />
+
+          <button className="btn btn-primary join-item" type="submit">
+            Generate Tour
+          </button>
+        </div>
+      </form>
+      <div className="mt-16">{tour ? <TourInfo tour={tour} /> : null}</div>
+    </>
+  );
+};
+
+export default NewTour;
