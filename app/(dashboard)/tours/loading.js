822e6d9d0bf79d4177ac6956536754a937ba6cd8
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-06-24 16:39:47 +0530

    added tours, new tour, and unsplash API

---
+++ app/(dashboard)/tours/loading.js
@@ -0,0 +1,5 @@
+const loading = () => {
+  return <span className="loading loading-lg"></span>;
+};
+
+export default loading;
--- utils/action.js
+++ utils/action.js
@@ -1,5 +1,6 @@
 "use server";
 import OpenAI from "openai";
+import prisma from "./db";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -23,10 +24,6 @@ export const generateChatResponse = async (chatMessages) => {
   }
 };
 
-export const getExistingTour = async ({ city, country }) => {
-  return null;
-};
-
 export const generateTourResponse = async ({ city, country }) => {
   const query = `Find a ${city} in this ${country}.
 If ${city} in this ${country} exists, create a list of things families can do in this ${city},${country}. 
@@ -37,7 +34,7 @@ Once you have a list, create a one-day tour. Response should be in the following
     "country": "${country}",
     "title": "title of the tour",
     "description": "description of the city and tour",
-    "stops": ["short paragraph on the stop 1 ", "short paragraph on the stop 2","short paragraph on the stop 3"]
+    "stops": ["stop name","stop name","stop name"]
   }
 }
 If you can't find info on exact ${city}, or ${city} does not exist, or it's population is less than 1, or it is not located in the following ${country} return { "tour": null }, with no additional characters.`;
@@ -60,6 +57,62 @@ If you can't find info on exact ${city}, or ${city} does not exist, or it's popu
   }
 };
 
+export const getExistingTour = async ({ city, country }) => {
+  return prisma.tour.findUnique({
+    where: {
+      city_country: {
+        city,
+        country,
+      },
+    },
+  });
+};
+
 export const createNewTour = async (tour) => {
-  return null;
+  return prisma.tour.create({
+    data: tour,
+  });
+};
+
+export const getAllTours = async (searchTerm) => {
+  if (!searchTerm) {
+    const tours = await prisma.tour.findMany({
+      orderBy: {
+        city: "asc",
+      },
+    });
+    return tours;
+  }
+  const tours = await prisma.tour.findMany({
+    where: {
+      OR: [
+        { city: { contains: searchTerm }, country: { contains: searchTerm } },
+      ],
+    },
+    orderBy: {
+      city: "asc",
+    },
+  });
+  return tours
+};
+
+export const getSingleTour = async (id) => {
+  return prisma.tour.findUnique({
+    where: {
+      id
+    }
+  })
+}
+
+export const generateTourImage = async ({ city, country }) => {
+  try {
+    const tourImage = await openai.images.generate({
+      prompt: `a panoramic view of the ${city} ${country}`,
+      n: 1,
+      size: '512x512',
+    });
+    return tourImage?.data[0]?.url;
+  } catch (error) {
+    return null;
+  }
 };
